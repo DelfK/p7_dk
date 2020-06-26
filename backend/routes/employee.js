783d622475024738c9d167138bd7f86b9decdb20
Delphine Klock <delphine.klock@gmail.com>
2020-06-26 18:21:10 +0200

    creating the model folder and employee model

--- backend/routes/employee.js
+++ backend/routes/employee.js
@@ -1,5 +1,8 @@
 const employeeRouter = require('express').Router();
 
+// import the employee model
+const Employee = require('../models/employee');
+
 // Login
 employeeRouter.get('/login', (req, res, next) => {
 res.send('Login')
